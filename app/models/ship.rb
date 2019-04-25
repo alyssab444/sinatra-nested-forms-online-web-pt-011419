0f32a8beb636b75bc8b1b34a1552b68bbc35887d
Alyssa Brown <alyssa.brown@live.com>
2019-04-25 23:42:38 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,12 @@
 class Ship
+  attr_accessor :name, :type, :booty 
+  @@all = []
+  def intialize(name, type, booty)
+    @name = name 
+    @type = type 
+    @booty = booty 
+    @@all << self 
+  end 
+  def self.all 
+    @@all 
 end
