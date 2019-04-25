ce99fc782b5c22c6352e3a51f4867c0fc2af2bb1
Alyssa Brown <alyssa.brown@live.com>
2019-04-25 23:41:14 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,9 +1,15 @@
 class Pirate
   attr_accessor :name, :weight, :height 
   
+  @@all = []
   def initalize(name, weight, height)
     @name = name 
     @weight = weight 
     @height = height 
+    @@all < self 
+  end 
+  
+  def self.all 
+    @@all 
   end 
 end
