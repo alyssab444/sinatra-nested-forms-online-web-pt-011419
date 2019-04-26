fb40eb793ac4b08785c9d3e71aa68be17ee0e2bd
Alyssa Brown <alyssa.brown@live.com>
2019-04-26 00:16:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,9 +7,9 @@ module FormsLab
      erb :root 
   end
   
-  get '/new' do 
-    erb ':pirates/new'
-  end 
+  get '/new' do
+      erb :'pirates/new'
+    end
 
   post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
